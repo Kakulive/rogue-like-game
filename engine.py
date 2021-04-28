8ee50b7b177e0a2aebc5c55b3a6b26d9c6d09a9b
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-04-28 22:41:41 +0200

    Gate 1 coords added. Player creation adaded. Put_player_on_board added, minor adjustments to other functions

--- engine.py
+++ engine.py
@@ -1,15 +1,36 @@
 import random
+import ui as ui
 
 MAP_WIDTH = 45
 MAP_HEIGHT = 45
-NUMBER_OF_MAP_SLICES_VER = 3
-NUMBER_OF_MAP_SLICES_HOR = 3
-MINIMUM_ROOM_WIDTH = 6
-MINIMUM_ROOM_HEIGHT = 6
+NO_OF_MAP_SLICES_VER = 3
+NO_OF_MAP_SLICES_HOR = 3
+MIN_ROOM_WIDTH = 6
+MIN_ROOM_HEIGHT = 6
 # FILLER = color("-", fg="red")
 # WALL_SYMBOL = "\033[91m" + "%" + "\033[0m"
 FILLER = "-"
 WALL_SYMBOL = "W"
+GATE_SYMBOL = "]"
+
+def create_player():
+    player = {"race": 'human', "hp":30, "atck":15, "load":150}
+
+    name = ui.get_single_input("What is your name, adventurer sheepo?: ")
+    player["name"] = name
+
+    available_icons = ["M","F","H","S"]
+    available_races = ["black_sheep", "normal_sheep", "rainbow_sheep", "elephant"]
+
+    player["icon"] = "P"
+    # '''
+    # Creates a 'player' dictionary for storing all player related informations - i.e. player icon, player position.
+    # Fell free to extend this dictionary!
+
+    # Returns:
+    # dictionary
+    # '''
+    return player
 
 def create_map(width, height):
     game_map = []
@@ -21,7 +42,7 @@ def create_map(width, height):
 
     return game_map
 
-def is_room_dim_valid(room_coords, room_width, room_height, max_width, max_height):
+def is_room_coord_valid(room_coords, room_width, room_height, max_width, max_height):
     if max_width - (room_coords[0]+room_width) > 0 and max_height - (room_coords[1]+room_height) > 0:
         return True
     else:
@@ -41,28 +62,28 @@ def create_rooms(game_map):
     start_y_coord = 0
     end_y_coord = MAP_HEIGHT//3
     
-    for _ in range(NUMBER_OF_MAP_SLICES_VER):
+    for _ in range(NO_OF_MAP_SLICES_VER):
         start_x_coord = 0
         end_x_coord = MAP_WIDTH//3
-        for _ in range(NUMBER_OF_MAP_SLICES_HOR):
-            room_width = random.choice(list(range(MINIMUM_ROOM_WIDTH,((MAP_WIDTH//NUMBER_OF_MAP_SLICES_HOR)-2))))
-            room_height = random.choice(list(range(MINIMUM_ROOM_HEIGHT,((MAP_HEIGHT//NUMBER_OF_MAP_SLICES_VER)-2))))
-            room_coords = (random.choice(list(range(start_x_coord,end_x_coord))),random.choice(list(range(start_y_coord,end_y_coord))))
-            while is_room_dim_valid(room_coords, room_width, room_height, end_x_coord, end_y_coord) == False:
+        for _ in range(NO_OF_MAP_SLICES_HOR):
+            room_width = random.choice(list(range(MIN_ROOM_WIDTH,((MAP_WIDTH//NO_OF_MAP_SLICES_HOR)-2))))
+            room_height = random.choice(list(range(MIN_ROOM_HEIGHT,((MAP_HEIGHT//NO_OF_MAP_SLICES_VER)-2))))
+            room_coords = (random.choice(list(range(start_x_coord,end_x_coord))) , random.choice(list(range(start_y_coord,end_y_coord))))
+            while is_room_coord_valid(room_coords, room_width, room_height, end_x_coord, end_y_coord) == False:
                 room_coords = (random.choice(list(range(start_x_coord,end_x_coord))),random.choice(list(range(start_y_coord,end_y_coord))))
 
             insert_room(room_coords, room_height, room_width, game_map, number_to_replace)
 
             number_to_replace = str(int(number_to_replace) + 1)
-            start_x_coord += MAP_WIDTH//NUMBER_OF_MAP_SLICES_HOR
-            end_x_coord += MAP_WIDTH//NUMBER_OF_MAP_SLICES_HOR
+            start_x_coord += MAP_WIDTH//NO_OF_MAP_SLICES_HOR
+            end_x_coord += MAP_WIDTH//NO_OF_MAP_SLICES_HOR
 
-        start_y_coord += MAP_HEIGHT//NUMBER_OF_MAP_SLICES_VER
-        end_y_coord += MAP_HEIGHT//NUMBER_OF_MAP_SLICES_VER
+        start_y_coord += MAP_HEIGHT//NO_OF_MAP_SLICES_VER
+        end_y_coord += MAP_HEIGHT//NO_OF_MAP_SLICES_VER
 
 def get_room_coordinates(game_map):
     rooms_coordinates = []
-    current_room_numbers = [str(x+1) for x in range(NUMBER_OF_MAP_SLICES_VER * NUMBER_OF_MAP_SLICES_HOR)]
+    current_room_numbers = [str(x+1) for x in range(NO_OF_MAP_SLICES_VER * NO_OF_MAP_SLICES_HOR)]
     for room in current_room_numbers:
         single_room = []
         for row_index in range(len(game_map)):
@@ -72,9 +93,29 @@ def get_room_coordinates(game_map):
         rooms_coordinates.append(single_room)
     return rooms_coordinates
 
-def gate_generator_room_1(rooms_coordinates):
-    pass
+def gate_generator_room_1(game_map, rooms_coordinates):
+    min_col = 9999
+    max_col = 0
+    max_row = 0
+    for coord in rooms_coordinates[0]:
+        if coord[1] > max_row:
+            max_row = coord[1]
 
+        if coord[0] > max_col:
+            max_col = coord[0]
+
+        if coord[0] < min_col:
+            min_col = coord[0]
+    
+    final_row = max_row + 1
+    final_col = random.choice(list(range(min_col,max_col)))
+
+    insert_into_map(game_map, GATE_SYMBOL, final_row, final_col)
+
+    return (final_col, final_row)
+
+def insert_into_map(game_map, symbol, x, y):
+    game_map[y][x] = symbol
 
 def put_player_on_board(board, player):
     '''
--- main.py
+++ main.py
@@ -2,34 +2,24 @@ import util as util
 import engine as engine
 import ui as ui
 
-PLAYER_ICON = '🐑'
-PLAYER_START_X = 3
-PLAYER_START_Y = 3
-
 MAP_WIDTH = 45
 MAP_HEIGHT = 45
 
-
-def create_player():
-    '''
-    Creates a 'player' dictionary for storing all player related informations - i.e. player icon, player position.
-    Fell free to extend this dictionary!
-
-    Returns:
-    dictionary
-    '''
-    pass
-
-
 def main():
-    # player = create_player()
     game_map = engine.create_map(MAP_WIDTH, MAP_HEIGHT)
     ui.print_map(game_map)
+
     engine.create_rooms(game_map)
     ui.print_map(game_map)
-    rooms_coordinates = engine.get_room_coordinates(game_map)
-    print(rooms_coordinates)
 
+    rooms_coordinates = engine.get_room_coordinates(game_map)
+    gate_1 = engine.gate_generator_room_1(game_map, rooms_coordinates)
+    ui.print_map(game_map)
+    
+    player = engine.create_player()
+    player_position = ui.put_player_on_board(game_map, player, rooms_coordinates)
+    ui.print_map(game_map)
+    
     # util.clear_screen()
     # is_running = True
     # while is_running:
@@ -42,7 +32,6 @@ def main():
     #     else:
     #         pass
     #     util.clear_screen()
-
 
 if __name__ == '__main__':
     main()
